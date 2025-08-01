e2c2ae3c8b4ec6155864bd3e75b9c2bc61617195
Andrew Moore <andrew.d.moore@proton.me>
2025-08-01 11:55:41 -0400

    Added ability to capture and shoot enemies.

--- src/game/arena.gd
+++ src/game/arena.gd
@@ -3,8 +3,37 @@ extends Node2D
 
 var game_time: float = 0
 
-@onready var player: CharacterBody2D = %Player
+@onready var player: Player = %Player
+
+
+func _ready() -> void:
+	player.draw_controller.line_complete.connect(on_line_complete)
 
 
 func _process(delta: float) -> void:
 	game_time += delta
+
+
+func on_line_complete(points: Array[Vector2], penalty: float) -> void:
+	var enemy_captured := false
+
+	for node in $Enemies.get_children():
+		if node is not Enemy:
+			continue
+
+		var enemy := node as Enemy
+		if enemy.is_inside_polygon(points):
+			enemy_captured = true
+			$Enemies.remove_child(enemy)
+			enemy.queue_free()
+			player.captured_enemy()
+
+	if enemy_captured:
+		player.draw_controller.active = false
+
+func on_player_shoot(direction: Vector2) -> void:
+	var new_projectile := preload("res://src/game/enemy_projectile.tscn").instantiate()
+	new_projectile.direction = direction
+	new_projectile.initial_position = player.position
+	new_projectile.position = player.position
+	$Projectiles.add_child(new_projectile)
--- src/game/player.gd
+++ src/game/player.gd
@@ -3,9 +3,16 @@ extends CharacterBody2D
 
 
 signal player_died()
+signal shoot(direction: Vector2)
 
 
 const SPEED := 180.0
+
+
+@onready var draw_controller: DrawController = $DrawController
+
+
+var captured_enemies := 0
 
 
 func _physics_process(_delta: float) -> void:
@@ -18,6 +25,24 @@ func _physics_process(_delta: float) -> void:
 		$AnimatedSprite2D.play("default")
 	move_and_slide()
 
+
+func _unhandled_input(event: InputEvent) -> void:
+	if captured_enemies == 0:
+		return
+
+	if event.is_action_pressed(&"shoot"):
+		var direction := get_local_mouse_position().normalized()
+		shoot.emit(direction)
+		captured_enemies -= 1
+		if captured_enemies == 0:
+			draw_controller.active = true
+		get_viewport().set_input_as_handled()
+
+
 func hit_by_cactus() -> void:
 	print("Oof ouch owie my bones.")
 	player_died.emit()
+
+
+func captured_enemy() -> void:
+	captured_enemies += 1
