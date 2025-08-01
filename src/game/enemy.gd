15de913a5ca1028c1eb421b53fc3c6e20d1264f7
kdreese <reesekyle1998@gmail.com>
2025-07-31 21:24:41 -0400

    Added enemy and basic spawning logic

---
+++ src/game/enemy.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+
+const SPEED := 80.0
+
+
+var is_lassoed := false
+
+
+func _physics_process(_delta: float) -> void:
+	# Don't apply this mopement if the enemy is lassoed
+	if not is_lassoed:
+		# Set direction to vector at the player
+		var player: CharacterBody2D = get_tree().get_first_node_in_group("Player")
+		var player_position := player.global_position
+		var direction := (player_position - position).normalized()
+		velocity = direction * SPEED
+		move_and_slide()
+	else:
+		# Set position to... slowly circling the player?
+		pass
--- src/game/player.gd
+++ src/game/player.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
 
-const SPEED := 400.0
+const SPEED := 180.0
 
 
-func _process(_delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var direction := Input.get_vector(&"move_left", &"move_right", &"move_up", &"move_down")
 	velocity = direction * SPEED
 	move_and_slide()
