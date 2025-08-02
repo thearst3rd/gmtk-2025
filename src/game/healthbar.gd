28fc3cb139ca586890df74d0baf3b404bbb5965e
kdreese <reesekyle1998@gmail.com>
2025-08-02 00:17:11 -0400

    Cleaned up array declaration

--- src/game/healthbar.gd
+++ src/game/healthbar.gd
@@ -3,17 +3,11 @@ extends HBoxContainer
 
 const MAX_HEALTH := 3
 
-var health_markers: Array[TextureRect]
+
 var healthy_texture := preload("res://assets/cowboy_hat.png")
 var unhealthy_texture := preload("res://icon.svg")
 
-@onready var health_marker: TextureRect = $HealthMarker
-@onready var health_marker_2: TextureRect = $HealthMarker2
-@onready var health_marker_3: TextureRect = $HealthMarker3
-
-
-func _ready() -> void:
-	health_markers.append_array([health_marker, health_marker_2, health_marker_3])
+@onready var health_markers: Array[TextureRect] = [$HealthMarker, $HealthMarker2, $HealthMarker3]
 
 
 func _on_player_player_hit(current_health: int) -> void:
