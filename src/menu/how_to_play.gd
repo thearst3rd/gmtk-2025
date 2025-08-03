6bd5d1d2ef972df4af9dbcc4316aac4c9f7d6293
Terry Hearst <thearst3rd@gmail.com>
2025-08-02 23:37:25 -0400

    Tweak help text and add 4th panel

--- src/menu/how_to_play.gd
+++ src/menu/how_to_play.gd
@@ -5,12 +5,13 @@ signal exited()
 
 
 const TEXT = [
-	"Capture enemies in your lasso by clicking and dragging with the mouse to draw a loop around them.",
-	"When you have an enemy capture, aim at a cactus, rock or other enemy and click to shoot.",
-	"If you draw a very round circle, your cursor will turn gold, and your projectiles may have extra effects.",
+	"Capture enemies in your lasso by clicking and dragging with the mouse to draw a loop around them. The loop has to be closed and fully contain the enemy.",
+	"When you have an enemy captured, aim at a cactus, rock or other enemy and click to shoot. You can capture multiple enemies in the same loop.",
+	"If the loop you drew is close to a perfect circle, your cursor will turn gold, and projectiles you throw will cause a chain reaction!",
+	"The longer you're alive, the more difficult it gets and enemy spawns will increase. Try to get as many points as you can!",
 ]
 
-const MAX_SCREENS = 3
+const MAX_SCREENS = 4
 
 
 var current_screen_idx = 0
@@ -19,9 +20,12 @@ var current_screen_idx = 0
 @onready var prev_button: Button = %PrevButton
 @onready var next_button: Button = %NextButton
 @onready var step_explanation: Label = %StepExplanation
+
 @onready var lasso_view: Node2D = %LassoView
 @onready var projectile_view: Node2D = %ProjectileView
 @onready var gold_view: Node2D = %GoldView
+@onready var difficulty_view: Node2D = $DifficultyView
+
 @onready var gold_cursor: AnimatedSprite2D = $GoldView/GoldCursor
 
 
@@ -30,7 +34,7 @@ func _ready() -> void:
 	lasso_view.hide()
 	projectile_view.hide()
 	gold_view.hide()
-	gold_cursor.play("default")
+	gold_cursor.play(&"default")
 
 
 func to_prev_screen() -> void:
@@ -48,25 +52,23 @@ func to_next_screen() -> void:
 func load_screen() -> void:
 	step_explanation.text = TEXT[current_screen_idx]
 
+	prev_button.disabled = current_screen_idx <= 0
+	next_button.disabled = current_screen_idx >= MAX_SCREENS - 1
+
+	$AnimationPlayer.stop()
+	lasso_view.hide()
+	projectile_view.hide()
+	gold_view.hide()
+	difficulty_view.hide()
+
 	if current_screen_idx == 0:
-		projectile_view.hide()
-		gold_view.hide()
-		prev_button.disabled = true
-		next_button.disabled = false
-		$AnimationPlayer.play("lasso_view")
+		$AnimationPlayer.play(&"lasso_view")
 	elif current_screen_idx == 1:
-		lasso_view.hide()
-		gold_view.hide()
-		prev_button.disabled = false
-		next_button.disabled = false
-		$AnimationPlayer.play("projectile_view")
+		$AnimationPlayer.play(&"projectile_view")
 	elif current_screen_idx == 2:
-		$AnimationPlayer.stop()
-		lasso_view.hide()
-		projectile_view.hide()
 		gold_view.show()
-		prev_button.disabled = false
-		next_button.disabled = true
+	elif current_screen_idx == 3:
+		$AnimationPlayer.play(&"difficulty_view")
 
 
 func focus() -> void:
